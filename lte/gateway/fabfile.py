a02c7a2d7fa65ea5431c4585dd53ef798a8e945e
Lars Kreutzer <lars.kreutzer@tngtech.com>
2022-05-23 16:09:11 +0200

    add timeout and generate bazel profile

--- lte/gateway/fabfile.py
+++ lte/gateway/fabfile.py
@@ -585,7 +585,10 @@ def _build_magma():
 def _build_magma_bazel():
     """ Build magma on AGW with bazel """
     with cd(r"$MAGMA_ROOT"):
-        run('bazel build \\...') 
+        run('sudo apt-get update -y') 
+        run('sudo apt-get install -y moreutils sysstat') 
+        run('timeout 2400 bash -c "while true; do echo AAAAAAAAAAAa; date; mpstat; free -m; iostat; sleep 5; done & bazel build //... --profile=bazel_profile 2>&1 | ts"')
+
 
 def _modify_for_bazel():
     """ Modify the service definitions to use the bazel-built executables """
